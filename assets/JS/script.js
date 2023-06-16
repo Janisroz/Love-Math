21c967b1178ca7c822a4d93cc3b4477618b41835
Janisroz <>
2023-06-16 19:51:53 +0000

    Display addition questopm to the user

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -10,10 +10,12 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
 });
 
 /**
@@ -21,11 +23,17 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the users answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
 
     // Creates two random numbers between 1 and 25
-    let num 1 = Math.floor(Math.random() * 25) + 1
-    let num 2 = Math.floor(Math.random() * 25) + 1;
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting`;
+    }
 }
 
 function checkAnswer() {
@@ -40,7 +48,10 @@ function incrementScore() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').innerHTML = operand1;
+    document.getElementById('operand2').innerHTML = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
